88e910011e12c97e2cdd8940fc089c4672aa5547
Denis Mamedov <denismj87@gmail.com>
2019-07-26 22:10:52 -0500

    Display image of digit from data set

--- NN/NN.py
+++ NN/NN.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as op
 import scipy.io as sio
+from PIL import Image
 
 dataset = sio.loadmat('ex4data1.mat', squeeze_me=True)
 weights = sio.loadmat('ex4weights.mat', squeeze_me=True)
@@ -18,7 +19,11 @@ dfX = pd.DataFrame(data=X, index=X[:, 0], columns=X[0, :])
 dfy = pd.DataFrame(data=y)
 
 print(Theta1.shape)
-print(Theta2)
+print(Theta2.shape)
 print(dfX.info())
 print(dfy.info())
 
+# check image in data set
+img = np.reshape(X[0, :], (20,20))
+im = Image.fromarray(img * 255)
+im.show()
