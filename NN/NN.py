1132b98b557bf4153f5a097ee190d9f99dde4e49
Denis Mamedov <denismj87@gmail.com>
2019-07-26 22:57:47 -0500

    Display rand set of images from data set in a row

--- NN/NN.py
+++ NN/NN.py
@@ -8,7 +8,6 @@ from PIL import Image
 dataset = sio.loadmat('ex4data1.mat', squeeze_me=True)
 weights = sio.loadmat('ex4weights.mat', squeeze_me=True)
 
-
 # Numpy for matrix operations
 X = dataset['X']
 y = dataset['y']
@@ -23,7 +22,21 @@ print(Theta2.shape)
 print(dfX.info())
 print(dfy.info())
 
+img1 = np.reshape(X[1, :], (20, 20))
+img2 = np.reshape(X[2, :], (20, 20))
+img_pair = np.hstack((img1, img2))
 # check image in data set
-img = np.reshape(X[0, :], (20,20))
-im = Image.fromarray(img * 255)
-im.show()
+
+
+# displays digits in a row , orientation is not right still
+def dispData(num_of_digits):
+    img_arr_i = np.reshape(X[1, :], (20, 20))
+    for i in range(num_of_digits):
+        temp_arr_i = np.reshape(X[np.random.randint(0, 4999), :], (20, 20))
+        img_arr_i = np.hstack((img_arr_i, temp_arr_i))
+
+    im = Image.fromarray(img_arr_i * 255)
+    im.show()
+
+
+dispData(20)
