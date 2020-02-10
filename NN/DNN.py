6cb045bee84695b4c96edb561233794769490d51
denmj <denismj87@gmail.com>
2020-02-10 14:14:12 -0600

    DNN backprop multiclass/binary

--- NN/DNN.py
+++ NN/DNN.py
@@ -1,7 +1,93 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy.io as sio
 from helper_funcs import *
-from sklearn.model_selection import train_test_split
+import scipy.io as sio
+from utils import *
+from sklearn.preprocessing import LabelEncoder
+
+
+# Digits
+dataset = sio.loadmat('C:/Users/u325539/Desktop/ML/proj/ML_regs/log_reg/ex3data1.mat', squeeze_me=True)
+
+# weights = sio.loadmat('log_reg/ex3weights.mat', squeeze_me=True)
+
+X_data_orig = dataset['X']  # [5000, 400]
+y_data_orig = dataset['y']  # [5000, 1]
+
+x_train_dig = X_data_orig.T
+y_train_dig = y_data_orig.reshape(5000, -1)
+ohe = OneHotEncoder(categories='auto')
+y_train_dig = ohe.fit_transform(y_train_dig).toarray()
+y_train_dig = y_train_dig.T
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+# Standardize data to have feature values between 0 and 1.
+train_x = train_x_flatten/255.
+test_x = test_x_flatten/255.
+
+print ("train_x's shape: " + str(train_x.shape))
+print ("test_x's shape: " + str(test_x.shape))
+print ("train_y's shape: " + str(train_y.shape))
+print ("test_y's shape: " + str(test_y.shape))
+
+layers_dims = [12288, 20, 7, 5, 1]  #4-layer model
+layers_dims_dig = [400, 64, 32, 32, 10]
+
+print ("train_x_dig's shape: " + str(x_train_dig.shape))
+print ("test_x_dig's shape: " + str(y_train_dig.shape))
+
+
+
+
+def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):  # lr was 0.009
+
+    np.random.seed(1)
+    costs = []  # keep track of cost
+
+    parameters = initialize_parameters_deep(layers_dims)
+    ### END CODE HERE ###
+
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        ### START CODE HERE ### (≈ 1 line of code)
+        AL, caches = L_model_forward(X, parameters)
+        ### END CODE HERE ###
+
+        # Compute cost.
+        ### START CODE HERE ### (≈ 1 line of code)
+        cost = compute_cost(AL, Y)
+        ### END CODE HERE ###
+
+        # Backward propagation.
+        ### START CODE HERE ### (≈ 1 line of code)
+        grads = L_model_backward(AL, Y, caches)
+        ### END CODE HERE ###
+
+        # Update parameters.
+        ### START CODE HERE ### (≈ 1 line of code)
+        parameters = update_parameters(parameters, grads, learning_rate)
+        ### END CODE HERE ###
+
+        # Print the cost every 100 training example
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+
+    # plot the cost
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
+# parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
+parameters_2 = L_layer_model(x_train_dig, y_train_dig, layers_dims_dig, num_iterations = 2500, print_cost = True)
 
--- helper_funcs.py
+++ helper_funcs.py
@@ -1,31 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
 from scipy.special import softmax as sf
-
-
-#
-# Data set
-#
-def random_pick(X_data, rand_size):
-    inx = np.random.randint(X_data.shape[0], size=rand_size)
-    rand_data_set = X_data[inx, :]
-    return rand_data_set
-
-
-#
-# Images
-#
-def show_images(X_data, cols, rows, cmap=None):
-    pic_pix_size = int(np.sqrt(X_data.shape[1]))
-
-    X_reshaped = X_data.reshape(X_data.shape[0], pic_pix_size, pic_pix_size, order='F')
-    fig = plt.figure(figsize=(20, 20))
-
-    for image_num in range(X_reshaped.shape[0]):
-        fig.add_subplot(rows, cols, image_num + 1)
-        plt.imshow(X_reshaped[image_num], cmap=cmap)
-    plt.show()
 
 
 #
@@ -52,120 +27,44 @@ def softmax(x):
     return softmax
 
 
-def relu(x, derivative=False):
-    relu = np.maximum(0, x)
+def relu(z, derivative=False):
+    relu = np.maximum(0, z)
     if derivative:
-        x[x<=0] = 0
-        x[x>0] = 1
-        relu = x
+        z[z<=0] = 0
+        z[z>0] = 1
+        relu = z
     return relu
+
+
+def relu_derivative(dA, cache):
+    Z = cache
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0
+    return dZ
+
+
+def sigmoid_derivative(dA, cache):
+    Z = cache
+    s = 1 / (1 + np.exp(-Z))
+    dZ = dA * s * (1 - s)
+    return dZ
 
 
 #
 # Helper func for models
 #
-def parameters_initialization(layers_dim, w_values=None):
-    parameters = {}
-    if w_values == "zeros":
-        for l in range(1, len(layers_dim)):
-            parameters['W'+str(l)] = np.zeros(shape=[layers_dim[l], layers_dim[l-1]])
-            parameters['b'+str(l)] = np.zeros(shape=[layers_dim[l], 1])
-    else:
-        for l in range(1, len(layers_dim)):
-            parameters['W' + str(l)] = np.random.randn(layers_dim[l], layers_dim[l - 1]) * 0.01
-            parameters['b' + str(l)] = np.zeros(shape=[layers_dim[l], 1])
-    return parameters
 
-
-def linear_activation(a, W, b, activation):
-    if activation == "sigmoid":
-        Z, linear_func_cache = linear_func(a, W, b)
-        activation_cache = Z
-        A = sigmoid(Z)
-    elif activation == "relu":
-        Z, linear_func_cache = linear_func(a, W, b)
-        activation_cache = Z
-        A = relu(Z)
-    elif activation == "softmax":
-        Z, linear_func_cache = linear_func(a, W, b)
-        activation_cache = Z
-        A = sf(Z)
-    cache = (linear_func_cache, activation_cache)
-    return A, cache
-
-
-def linear_backward(dZ, cache):
-    A_prev, W, b = cache
-    m = A_prev.shape[1]
-
-    dW = (1 / m) * np.dot(dZ, A_prev.T)
-    db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-    dA_prev = np.dot(W.T, dZ)
-
-    return dA_prev, dW, db
-
-
-def linear_activation_backward(dA, Y, cache, activation=None):
-    linear_cache, activation_cache = cache
-    print(dA.shape)
-    print(Y.shape)
-    if activation == "relu":
-        dZ = dA * relu(activation_cache, derivative=True)
-        dA_prev, dW, db = linear_backward(dZ, linear_cache)
-
-    if activation == "sigmoid":
-        dZ = dA * sigmoid(activation_cache, derivative=True)
-        dA_prev, dW, db = linear_backward(dZ, linear_cache)
-    else:
-
-        dZ = dA - Y
-        dA_prev, dW, db = linear_backward(dZ, linear_cache)
-    return dA_prev, dW, db
-
-
-def linear_model_backward(AL, Y, caches):
-    grads = {}
-    L = len(caches)
-    m = AL.shape[1]
-    # Y = Y.reshape(AL.shape)
-    dAL = -(np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
-    current_cache = caches[L - 1]
-    grads["dA" + str(L-1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, Y, current_cache)
-
-    for l in reversed(range(L - 1)):
-
-        current_cache = caches[l]
-        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 1)], Y, current_cache, 'relu')
-        grads["dA" + str(l)] = dA_prev_temp
-        grads["dW" + str(l + 1)] = dW_temp
-        grads["db" + str(l + 1)] = db_temp
-    return grads
-
-
-def linear_activation_forward(X, params):
-    A = X
-    caches = []
-    L = len(params) // 2
-    for l in range(1, L):
-        A_previous = A
-        A, cache = linear_activation(A_previous, params["W" + str(l)], params["b" + str(l)], "relu")
-        caches.append(cache)
-    AL, cache = linear_activation(A, params["W" + str(L)], params["b" + str(L)], "softmax")
-
-    caches.append(cache)
-    return AL, caches
-
-
-def compute_cost(Y_hat, Y):
-    # y_train_reshaped = Y.reshape(len(Y), 1)
-    # ohe = OneHotEncoder(categories='auto')
-    # y_train_reshaped = ohe.fit_transform(y_train_reshaped).toarray()
+def compute_cost(AL, Y):
+    epsilon = 1e-5
+    # Y = Y.reshape(len(Y), 1)
     # y_train_reshaped = y_train_reshaped.T
+    # ohe = OneHotEncoder(categories='auto')
+    # Y = ohe.fit_transform(Y).toarray()
+    # Y = Y.T
     m = 5000
-    print(Y_hat.shape)
     print(Y.shape)
-    cost = np.mean(-1 / m * np.sum(Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat), axis=1, keepdims=True))
-
+    print(AL.shape)
+    cost = np.mean(-1 / m * np.sum(Y.T * np.log(AL.T) + (1 - Y.T) * np.log(1 - AL.T), axis=1, keepdims=True))
     cost = np.squeeze(cost)
     return cost
 
@@ -250,35 +149,4 @@ def parameters_update(parameters, grads, alpha):
     for l in range(L):
         parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - alpha * grads["dW" + str(l + 1)]
         parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - alpha * grads["db" + str(l + 1)]
-    return parameters
-
-
-def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):  # lr was 0.009
-
-    np.random.seed(1)
-    costs = []
-    parameters = parameters_initialization(layers_dims)
-
-    # Loop (gradient descent)
-    for i in range(0, num_iterations):
-
-        AL, caches = linear_activation_forward(X.T, parameters)
-        cost = compute_cost(AL, Y)
-        grads = linear_model_backward(AL, Y, caches)
-
-        parameters = parameters_update(parameters, grads, learning_rate)
-
-        # Print the cost every 100 training example
-        if print_cost and i % 100 == 0:
-            print("Cost after iteration %i: %f" % (i, cost))
-        if print_cost and i % 100 == 0:
-            costs.append(cost)
-
-    # plot the cost
-    plt.plot(np.squeeze(costs))
-    plt.ylabel('cost')
-    plt.xlabel('iterations (per hundreds)')
-    plt.title("Learning rate =" + str(learning_rate))
-    plt.show()
-
     return parameters
