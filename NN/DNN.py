204d41341fa62679f664df10659f442bc5f57d9d
denmj <denismj87@gmail.com>
2020-02-10 14:25:18 -0600

    _

--- NN/DNN.py
+++ NN/DNN.py
@@ -1,6 +1,8 @@
+from NN.utils import initialize_parameters_deep, L_model_forward, L_model_backward, update_parameters, load_data
 from helper_funcs import *
 import scipy.io as sio
 from utils import *
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -39,40 +41,16 @@ print ("train_x_dig's shape: " + str(x_train_dig.shape))
 print ("test_x_dig's shape: " + str(y_train_dig.shape))
 
 
-
-
-def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):  # lr was 0.009
+def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):
 
     np.random.seed(1)
     costs = []  # keep track of cost
-
     parameters = initialize_parameters_deep(layers_dims)
-    ### END CODE HERE ###
-
-    # Loop (gradient descent)
     for i in range(0, num_iterations):
-
-        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
-        ### START CODE HERE ### (≈ 1 line of code)
         AL, caches = L_model_forward(X, parameters)
-        ### END CODE HERE ###
-
-        # Compute cost.
-        ### START CODE HERE ### (≈ 1 line of code)
         cost = compute_cost(AL, Y)
-        ### END CODE HERE ###
-
-        # Backward propagation.
-        ### START CODE HERE ### (≈ 1 line of code)
         grads = L_model_backward(AL, Y, caches)
-        ### END CODE HERE ###
-
-        # Update parameters.
-        ### START CODE HERE ### (≈ 1 line of code)
         parameters = update_parameters(parameters, grads, learning_rate)
-        ### END CODE HERE ###
-
-        # Print the cost every 100 training example
         if print_cost and i % 100 == 0:
             print ("Cost after iteration %i: %f" %(i, cost))
         if print_cost and i % 100 == 0:
