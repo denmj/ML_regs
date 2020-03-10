13fab274b94000ffa33f2486d991f6250e007b15
denmj <denismj87@gmail.com>
2020-03-09 21:52:44 -0500

    ResNet keras model

--- CNN/test_model.py
+++ CNN/test_model.py
@@ -5,35 +5,19 @@ import numpy as np
 from matplotlib import pyplot
 # Open the image form working directory
 
-# image = Image.open('C:/Users/denis/Desktop/ML/ML_regs/CNN/one_.jpg')
-# image = image.resize((64, 64))
-#
-# image = np.asarray(image)
-# image = image.reshape(1, image.shape[0],
-#                       image.shape[1],
-#                       image.shape[2])
-# print(image.shape)
+image = Image.open('C:/Users/denis/Desktop/ML/ML_regs/CNN/five_.jpg')
+image = image.resize((64, 64))
 
-X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
-
-print(X_train_orig.shape, Y_train_orig.shape, X_test_orig.shape, Y_test_orig.shape, classes)
-X_train = X_train_orig / 255.
-X_test = X_test_orig / 255.
-# Y_train = convert_to_one_hot(Y_train_orig, 6).T
-# Y_test = convert_to_one_hot(Y_test_orig, 6).T
-Y_train = Y_train_orig.T
-Y_test = Y_test_orig.T
+image = np.asarray(image)
+image = image.reshape(1, image.shape[0],
+                      image.shape[1],
+                      image.shape[2])
+print(image.shape)
 
 checkpoint_path = 'C:/Users/denis/Desktop/ML/ML_regs/CNN/cp.ckpt'
 cnn_model = create_model()
 
 cnn_model.load_weights(checkpoint_path)
-loss, acc = cnn_model.evaluate(X_test, Y_test)
-
-print(loss, acc)
-t = np.asarray(X_test[15])
-t = t.reshape(1, 64, 64, 3)
-pred = cnn_model.predict(t)
+pred = cnn_model.predict(image)
 
 print(np.argmax(pred, 1))
-print(Y_test[15])
--- t_flow/test.py
+++ t_flow/test.py
@@ -1,71 +1,16 @@
 import numpy as np
-from scipy.stats import entropy
+import matplotlib.pyplot as plt
+import scipy.stats as stat
 
 
+sent = [5, 7, 13, 9, 1, 13]
+rec = [10, 12, 14, 18, 6, 4]
 
-"""
-Fibonacci
+fig = plt.figure(figsize=(10, 4))
 
-"""
-
-def fib (n):
-    if n == 1:
-        return  0
-    elif n == 2:
-        return 1
-
-    else:
-        return fib(n-1) + fib(n-2)
-
-"""
-    Task 
-    Given a orig_dict - orig_dict =  {'Input.txt': 'Randy', 'Code.py': 'Stan', 'Output.txt': 'Randy'}
-
-    Modify it as following 
-    {'Randy': ['Input.txt', 'Output.txt'], 'Stan': ['Code.py']}
-
-"""
-
-orig_dict =  {'Input.txt': 'Randy', 'Code.py': 'Stan', 'Output.txt': 'Randy'}
-
-def t_fucn(dict):
-    s = set(val for val in dict.values())
-    t = {}
-    for uniq_name in s:
-        print(uniq_name)
-        value_list = []
-        for file in dict:
-            print(file)
-            if uniq_name == dict[file]:
-                value_list.append(file)
-        t.update({uniq_name: value_list})
-    return t
-
-"""
-
-["vanilla", "chocolate"], ["chocolate sauce"]
-return combination of topping and base
-
-"""
-list_1 = ["vanilla", "chocolate"]
-list_2 = ["chocolate sauce"]
+plt.scatter(sent, rec)
+plt.show()
 
 
-def icec(l1, l2):
-    comb_list = []
-    for e1 in l1:
-        for e2 in l2:
-            comb_list.append([e1, e2])
-
-    print(comb_list)
-
-
-
-"""
-Entropy
-"""
-
-value, counts = np.unique()
-
-def ent(data):
-    pass
+corr_cef = np.corrcoef(sent, rec)
+print(corr_cef)
