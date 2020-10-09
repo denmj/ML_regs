b316da2fb3645313f6e09d45f65951b3a8a17c40
denmj <denismj87@gmail.com>
2020-10-09 14:06:00 -0500

    lenet changes

--- CNN/LeNet5_.py
+++ CNN/LeNet5_.py
@@ -1,43 +1,62 @@
-import numpy as np
-
-import tensorflow as tf
+from CNN.BaseModel import BaseModel
+from tensorflow.keras.models import model_from_json
 from tensorflow import keras
 from tensorflow.keras import layers
-
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
-x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
-x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 
-# Classic LeNet5
-input_shape = (28, 28, 1)
-num_classes = 10
+class LeNet(BaseModel):
+    def __init__(self, input_shape, num_classes):
+        super(LeNet, self).__init__()
+        self.build_model(input_shape, num_classes)
+        self.hist = []
 
-inputs = keras.Input(shape=input_shape)
-x = layers.Conv2D(6, kernel_size=(3, 3), activation='relu')(inputs)
-x = layers.AveragePooling2D()(x)
-x = layers.Conv2D(16, kernel_size=(3, 3), activation='relu')(x)
-x = layers.AveragePooling2D()(x)
-x = layers.Flatten()(x)
-x = layers.Dense(120, activation='relu')(x)
-x = layers.Dense(84, activation='relu')(x)
-out = layers.Dense(num_classes, activation='softmax')(x)
+    def build_model(self, input_shape, num_classes):
+        inputs = keras.Input(shape=input_shape)
+        x = layers.Conv2D(6, kernel_size=(3, 3), activation='relu')(inputs)
+        x = layers.AveragePooling2D()(x)
+        x = layers.Conv2D(16, kernel_size=(3, 3), activation='relu')(x)
+        x = layers.AveragePooling2D()(x)
+        x = layers.Flatten()(x)
+        x = layers.Dense(120, activation='relu')(x)
+        x = layers.Dense(84, activation='relu')(x)
+        out = layers.Dense(num_classes, activation='softmax')(x)
 
+        self.model = keras.Model(inputs, out)
+        self.model.compile(loss=keras.losses.categorical_crossentropy,
+                           optimizer=keras.optimizers.Adam(),
+                           metrics=['accuracy'])
 
-model = keras.Model(inputs, out)
-model.summary()
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy",  metrics=['accuracy'])
+    def fit(self, x_train, y_train, epochs, callbacks, batch_size=128, plot=False):
+        self.hist = self.model.fit_generator(x_train, epochs=epochs, callbacks=callbacks,
+                                             validation_data=y_train)
 
-callbacks = [
-    keras.callbacks.ModelCheckpoint(
-        filepath='/resources/Machine-Learning-models/CNN_keras/',
-        save_freq='epoch')
-]
+        if plot:
+            plt.plot(self.hist.history['accuracy'])
+            plt.plot(self.hist.history['val_accuracy'])
+            plt.title('model accuracy')
+            plt.ylabel('accuracy')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'test'], loc='upper left')
+            plt.show()
+            # summarize history for loss
+            plt.plot(self.hist.history['loss'])
+            plt.plot(self.hist.history['val_loss'])
+            plt.title('model loss')
+            plt.ylabel('loss')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'test'], loc='upper left')
+            plt.show()
 
-# Train the model for 1 epoch from Numpy data
-batch_size = 64
-print("Fit on NumPy data")
-history = model.fit(x_train, y_train, batch_size=batch_size, epochs=2, callbacks= callbacks, validation_split = 0.2)
+    def evaluate(self, x_test, y_test):
+        scores = self.model.evaluate(x_test, y_test)
+        print('Test loss:', scores[0])
+        print('Test accuracy:', scores[1])
+
+    def save(self, checkpoint_path):
+        pass
+
+    def summary(self):
+        self.model.summary()
