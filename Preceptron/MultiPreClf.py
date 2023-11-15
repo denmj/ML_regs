0af17e151c029e0a61c3ef603b73a2fabbebdc41
denmj <denismj87@gmail.com>
2023-11-14 22:52:02 -0600

    activation and init

--- Preceptron/MultiPreClf.py
+++ Preceptron/MultiPreClf.py
@@ -32,8 +32,8 @@ class MultilayerPerceptron(object):
 
     def __init__(self, alpha=0.01, 
                  n_iterations=50,
-                 input_layers=784, 
-                 hidden_layers=[128, 64],
+                 input_layer_size=784, 
+                 hidden_layers_size=[128, 64],
                  n_outputs= 10):
         
         # alpha is the learning rate
@@ -43,7 +43,10 @@ class MultilayerPerceptron(object):
         self.n_iterations = n_iterations
 
         # input_layers is usually the number of features (MNIST: 784)
-        self.input_layers = input_layers
+        self.input_layer_size = input_layer_size
+
+        # hidden_layers is a list of the number of neurons in each hidden layer
+        self.hidden_layers_size = hidden_layers_size
 
         # n_outputs is the number of output neurons (MNIST: 10)
         self.n_outputs = n_outputs
@@ -52,11 +55,33 @@ class MultilayerPerceptron(object):
         self.bias = []
         
         # loop initialization
-        layer_sizes = [input_layers] + hidden_layers + [n_outputs]
+        self.layer_sizes = [self.input_layer_size] + self.hidden_layers_size + [self.n_outputs]
 
-        for i in range(len(layer_sizes)-1):
-            self.weights.append(np.random.randn(layer_sizes[i], layer_sizes[i+1]))
-            self.bias.append(np.zeros((1, layer_sizes[i+1])))
+        for i in range(len(self.layer_sizes)-1):
+            self.weights.append(np.random.randn(self.layer_sizes[i], self.layer_sizes[i+1]))
+            self.bias.append(np.zeros((1, self.layer_sizes[i+1])))
+
+    
+    # feed forward
+    def forward_propagation(self, X):
+        pass
+
+
+    # activation function - sigmoid 
+    def sigmoid(self, z):
+        return 1.0/(1.0+np.exp(-z))
+    
+    # derivative of sigmoid function 
+    def sigmoid_prime(self, z):
+        return self.sigmoid(z)*(1-self.sigmoid(z))
+    
+    # activation function - relu
+    def relu(self, z):
+        return np.maximum(0, z)
+
+    # derivative of relu function
+    def relu_prime(self, z):
+        return (z > 0).astype(int)
 
 
 
